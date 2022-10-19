3d3e5bae4f4c1ca8dde6b261db9f42393fc338e1
moddas311 <moddasir311@gmail.com>
2022-10-20 03:18:02 +0600

    news.json modified

--- index.js
+++ index.js
@@ -6,11 +6,35 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const news = require('./data/news.json');
 const categories = require('./data/categories.json');
+
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(news => news._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(news => news.category_id === id)
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
 
 app.get('/', (req, res) => {
     res.send('News API Ruing');
 });
+
 
 app.get('/news-category', (req, res) => {
     res.send(categories);
